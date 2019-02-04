aff7a4acc783e114040e114f9d73e2467321f723
Daniel Bendemagh <daniel.bendemagh@laposte.net>
2019-02-04 23:32:01 +0100

    Initial Commit

---
+++ Reciplease/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reciplease
+//
+//  Created by Daniel BENDEMAGH on 04/02/2019.
+//  Copyright © 2019 Daniel BENDEMAGH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
